01c26d1c4885616a549fa7947ef872d37405d18b
CRAIG KING <mavrrick58@gmail.com>
2018-12-23 14:21:44 -0500

    Notification correction

--- smartapps/mavrrick/adt-home-life-alert-action.src/adt-home-life-alert-action.groovy
+++ smartapps/mavrrick/adt-home-life-alert-action.src/adt-home-life-alert-action.groovy
@@ -490,7 +490,8 @@ def adtActionHandler() {
 					log.debug "Ignoring unexpected Light Action type."
         			log.debug "Light Action ${lightaction.value} detected"
                     break
-			} 		
+			}
+        sendnotification()
 		if (settings.recordCameras)
 			{
 		cameraRecord()
